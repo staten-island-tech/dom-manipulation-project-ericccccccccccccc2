8734cb2a993183e7ad0b0a2e509ebf0b4ee0c1e2
ericccccccccccccc2 <113203325+ericccccccccccccc2@users.noreply.github.com>
2022-11-10 14:01:35 -0500

    asdfasd

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const DOMSelectors = {
   thing: document.getElementById("poopy"),
   body: document.getElementById("body"),
-  selector: document.getElementById("select")
+  selector: document.getElementById("select"),
+  history: document.getElementById("history"),
 };
 
 function nowred(e) {
@@ -9,6 +10,7 @@ function nowred(e) {
   e.style.fontSize = "50px";
 }
 
+nowred(DOMSelectors.hist);
 nowred(DOMSelectors.thing);
 
 function redbkground(e) {
@@ -16,11 +18,31 @@ function redbkground(e) {
 }
 
 function readOper() {
-return document.getElementById("oper").value;
+  return document.getElementById("oper").value;
 }
 
+function add(x, y) {
+  return x + y;
+}
+
+function subtract(x, y) {
+  return x - y;
+}
+
+function multiply(x, y) {
+  return x * y;
+}
+
+function divide(x, y) {
+  return x / y;
+}
 
 function mathTime() {
   var operator = readOper();
-  console.log(operator);
+  if ((operator = "+")) {
+    let in1 = parseInt(document.getElementById("num1").value);
+    let in2 = parseInt(document.getElementById("num2").value);
+    let out = add(in1, in2);
+    console.log(out);
+  }
 }
