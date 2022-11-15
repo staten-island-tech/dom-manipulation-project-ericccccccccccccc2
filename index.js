5d083b5f1e2e4d68a1202829f0d604d19a799c15
ericccccccccccccc2 <113203325+ericccccccccccccc2@users.noreply.github.com>
2022-11-14 23:31:15 -0500

    i hate arrays

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const DOMSelectors = {
   body: document.getElementById("body"),
   selector: document.getElementById("select"),
   history: document.getElementById("history"),
-  button: document.getElementById("button")
+  button: document.getElementById("button"),
+  clear: document.getElementById("clear"),
 };
 
 function redbkground(e) {
@@ -34,23 +35,28 @@ function mathTime() {
   let operator = readOper();
   let in1 = parseInt(document.getElementById("num1").value);
   let in2 = parseInt(document.getElementById("num2").value);
-  console.log(operator);
   if ((operator === `+`)) {
-    return `<p>${in1} + ${in2} = ${add(in1, in2)}</p>`;
+    return `<p class="histtext">${in1} + ${in2} = ${add(in1, in2)}</p>`;
   }  
   else if ((operator === `-`)) {
-    return `<p>${in1} - ${in2} = ${subtract(in1, in2)}</p>`;
+    return `<p class="histtext">${in1} - ${in2} = ${subtract(in1, in2)}</p>`;
   } 
    else if ((operator === `x`)) {
-    return `<p>${in1} x ${in2} = ${multiply(in1, in2)}</p>`;
+    return `<p class="histtext">${in1} x ${in2} = ${multiply(in1, in2)}</p>`;
   }
   else {
-    return `<p>${in1} / ${in2} = ${divide(in1, in2)}</p>`;
+    return `<p class="histtext">${in1} / ${in2} = ${divide(in1, in2)}</p>`;
   }
 }
 
 DOMSelectors.button.addEventListener("click", function() {
   let out = mathTime();
-  console.log(out);
   DOMSelectors.history.insertAdjacentHTML('afterend', out)
 });
+
+
+DOMSelectors.clear.addEventListener("click", function() {
+  let arraynow = Array.from(document.getElementsByClassName("histtext"));
+  arraynow.remove();
+});
+
